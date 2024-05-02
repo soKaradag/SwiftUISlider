558498cb4f23d1fa7e31db40e2f2ef133291a91d
soKaradag <so.karadag@gmail.com>
2024-05-02 22:59:34 +0300

    Initial Commit

---
+++ ImageSlider/ImageSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSliderApp.swift
+//  ImageSlider
+//
+//  Created by Serdar Onur KARADAĞ on 2.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
